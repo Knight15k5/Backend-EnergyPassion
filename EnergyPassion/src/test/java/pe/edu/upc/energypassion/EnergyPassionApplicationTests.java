448f9c5c1e4d44a43041bd5410bfb5861b423398
Miguel Alonso Clavo Bardales <105908010+Knight15k5@users.noreply.github.com>
2022-10-25 11:20:22 -0500

    Trainer
    Se añadió las funcionalidades a la entidad Trainer

---
+++ EnergyPassion/src/test/java/pe/edu/upc/energypassion/EnergyPassionApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.energypassion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyPassionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
