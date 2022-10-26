9918f60cfd76d6c2fbf3893176d9fe7f99b24445
Andrew <46905711+an-lyzer@users.noreply.github.com>
2022-10-26 18:39:49 -0500

    Energy Passion-Suscripcion TB3
    La carpeta del main mas la suscripción

---
+++ EnergyPassion/src/main/java/pe/edu/upc/energypassion/EnergyPassionApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.energypassion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyPassionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyPassionApplication.class, args);
+	}
+
+}
